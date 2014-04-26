554f4e9769c12059337bc83423b5c35dbc1bae6b
fozy81 <fozy81@hotmail.com>
2014-04-26 09:32:36 +0100

    updated files

---
+++ Rscripts/server.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(RCurl)
+require(rCharts)
+library(reshape)
+
+d <- read.csv("duddingston_survey.csv")
+djson <- toJSONArray2(d, json = F)
+
+
+
+
+shinyServer(function(input, output) {
+  
+  mapLatLon <- reactive({
+    eval(parse(text=paste("d[d$Site == \"", input$dataset, "\", 10:11]",sep="")))
+  })
+  
+  
+  output$myMap <- renderMap({
+    map <- Leaflet$new()
+    map$geoJson(toGeoJSON(djson, lat = 'realLat', lon = 'realLon'),
+                onEachFeature = '#! function(feature, layer){
+                layer.bindPopup(feature.properties.Site)
+  } !#',
+                pointToLayer =  "#! function(feature, latlng){
+               return L.circleMarker(latlng, {
+               radius: 4,
+               fillColor: feature.properties.Color || 'red',    
+               color: '#000',
+               weight: 1,
+               fillOpacity: 0.8,
+               })
+               } !#"
+                
+    )
+    
+    latLon <- mapLatLon()
+    map$setView(c(latLon[,2], latLon[,1]), zoom = 15)
+    map$tileLayer("http://{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png")
+    map$set(height = '250px', width = '250px')
+    map
+  })
+  
+  
+  output$chart <- renderChart({
+    d <- d[d$LevelFormatted > "0",1:14] # removes '0' level incidents
+    d$Year <- as.numeric(d$Year)
+    n1 <- nPlot(LevelFormatted ~ Year, group = 'Location', data = d, type = "multiBarChart")
+    n1$addParams(dom = 'chart')
+    return(n1)
+  })
+  
+  
+  
+})
