ff2858aaec86bbd6e99fc38d64d41d9cb93002b7
fozy81 <fozy81@hotmail.com>
2014-04-28 20:41:30 +0100

    update

--- Rscripts/ui.R
+++ Rscripts/ui.R
@@ -2,17 +2,17 @@
 d <- read.csv("duddingston_survey.csv")
 
 shinyUI(pageWithSidebar(
-  
+  headerPanel("Nuclear Incidents"),
   sidebarPanel(
     selectInput("dataset", "Choose a site:", 
                 sort(paste(unique(sort(d$Location.of.the.space))))) 
     ),
-  mainPanel(
-    
-        tabsetPanel(  
-      tabPanel("Map",
+  mainPanel( 
+             tabsetPanel(
+      tabPanel("Map"),
+      tabPanel(  
                         leafletMap(
-                          "map1", "100%", "100%",
+                          "map1", "100%", "400",
                           initialTileLayer = "http://{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
                           initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
                           options=list(
@@ -22,6 +22,7 @@ shinyUI(pageWithSidebar(
                           )
                         )
            )
+             )
     )
   )
-))
+)
