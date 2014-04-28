416dbe60c5e06bd8b3f0cda9f23072e1e1f71ffc
fozy81 <fozy81@hotmail.com>
2014-04-28 20:18:10 +0100

    update

--- Rscripts/server.R
+++ Rscripts/server.R
@@ -11,45 +11,7 @@ djson <- toJSONArray2(d, json = F)
 
 shinyServer(function(input, output, session) {
   
-  # Create the map 
-map <- createLeafletMap(session, "map")
-
-
-
+  map1 <- createLeafletMap(session, 'map1')
   
-  mapLatLon <- reactive({
-    eval(parse(text=paste("d[d$Site == \"", input$dataset, "\", 10:11]",sep="")))
-  })
-  
-  
-  output$myMap <- renderMap({
-    map <- Leaflet$new()
-    map$geoJson(toGeoJSON(djson, lat = 'realLat', lon = 'realLon'),
-                onEachFeature = '#! function(feature, layer){
-                layer.bindPopup(feature.properties.Site)
-  } !#',
-                pointToLayer =  "#! function(feature, latlng){
-               return L.circleMarker(latlng, {
-               radius: 4,
-               fillColor: feature.properties.Color || 'red',    
-               color: '#000',
-               weight: 1,
-               fillOpacity: 0.8,
-               })
-               } !#"
-                
-    )
-    
-    latLon <- mapLatLon()
-    map$setView(c(latLon[,2], latLon[,1]), zoom = 15)
-    map$tileLayer("http://{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png")
-    map$set(height = '250px', width = '250px')
-    map
-  })
-  
-  
-
-  
-  
-  
-})
+}
+)
--- Rscripts/ui.R
+++ Rscripts/ui.R
@@ -3,31 +3,25 @@ d <- read.csv("duddingston_survey.csv")
 
 shinyUI(pageWithSidebar(
   
-  # Application title
-  headerPanel("Localtivate"),
-  
-  # Sidebar with controls to select a dataset and specify the number
-  # of observations to view
   sidebarPanel(
     selectInput("dataset", "Choose a site:", 
                 sort(paste(unique(sort(d$Location.of.the.space))))) 
-  ),
-
-  #  showOutput('myMap', 'leaflet')),
-  
+    ),
   mainPanel(
-    tabsetPanel(
-      tabPanel("Map"),
-      tabPanel( leafletMap("map", width="100%", height="100%",
-                           initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-                           initialTileLayerAttribution = HTML('Map data from<a href=\"http://openstreetmap.org\">OpenStreetMap</a>\n contributors, Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
-                           options=list(
-                             center = c(37.45, -93.85),
-                             zoom = 4,
-                             maxBounds = list(list(15.961329,-129.92981), list(52.908902,-56.80481)) 
+    
+        tabsetPanel(  
+      tabPanel("Map",
+                        leafletMap(
+                          "map1", "100%", "100%",
+                          initialTileLayer = "http://{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+                          initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
+                          options=list(
+                            center = c(49.25, 16),
+                            zoom = 4,
+                            maxBounds = list(list(17, -180), list(59, 180))
+                          )
+                        )
+           )
     )
-  )
-      )
-  )
   )
 ))
